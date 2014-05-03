7796ee31ed69170276aec0502d9e7190f4717bcb
Viktor Petersson <vpetersson@wireload.net>
2014-05-03 13:06:47 +0200

    Minor refactoring. Adds support for tags as a group in Ansible.

--- ansiblecs.py
+++ ansiblecs.py
@@ -31,32 +31,42 @@ db_store = '/tmp/ansible-cloudsigma.db'
 
 
 def refresh_db():
-    server = cloudsigma.resource.Server()
-    server_list = server.list()
-    server_db = {}
+    ansible_db = {}
+    running_uuid = []
 
-    for i in server_list:
-        if i['status'] == 'running':
-            name = i['name']
-            ipv4 = i['runtime']['nics'][0]['ip_v4']['uuid']
-            server_db[name] = {'ip': ipv4}
+    get_servers = cloudsigma.resource.Server()
+    server_list = get_servers.list()
+    get_tags = cloudsigma.resource.Tags()
+    tag_list = get_tags.list()
 
-    pickle.dump(server_db, open(db_store, 'wb'))
+    for server in server_list:
+        if server['status'] == 'running':
+            print 'Adding %s to inventory...' % server['name']
+
+            name = server['name']
+            ipv4 = server['runtime']['nics'][0]['ip_v4']['uuid']
+            running_uuid.append(server['uuid'])
+            ansible_db[name] = {'ansible_ssh_host': ipv4}
+
+    for tag in tag_list:
+        print 'Found tag %s' % tag['name']
+
+        tag_name = tag['name']
+        servers_tagged = []
+        for server in tag['resources']:
+            if (server['res_type'] == 'servers' and server['uuid'] in running_uuid):
+                for s in server_list:
+                    if s['uuid'] == server['uuid']:
+                        print 'Found %s tagged with %s...' % (s['name'], tag['name'])
+                        servers_tagged.append(s['name'])
+            ansible_db[tag_name] = {'hosts': servers_tagged}
+
+    pickle.dump(ansible_db, open(db_store, 'wb'))
 
 
 def list_hosts():
-    server_db = pickle.load(open(db_store, 'rb'))
-
-    # Format the output to follow the expected input.
-    def format_output():
-        formatted = {}
-        for i in server_db:
-            formatted[i] = {
-                'ansible_ssh_host': server_db[i]['ip']
-            }
-        return formatted
-
-    return format_output()
+    ansible_db = pickle.load(open(db_store, 'rb'))
+    return ansible_db
 
 
 def get_host():
